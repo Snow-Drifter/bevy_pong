95d0e96275e3e268b3cbdcae2222aa25664d4f79
BrilliantSnow <snowbastion@gmail.com>
2025-03-23 16:33:12 -0500

    disabled pipelined rendering due to x11 inconsistencies

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::pipelined_rendering::PipelinedRenderingPlugin};
 
-mod ball;
 mod background;
+mod ball;
 mod paddle;
 mod pixel_grid;
 mod scoreboard;
@@ -12,27 +12,35 @@ use scoreboard::{ScoreBoard, ScoreEvent};
 fn main() {
     App::new()
         .add_plugins(
-            DefaultPlugins.set(window::get_window_settings()),
+            DefaultPlugins
+                .set(window::get_window_settings())
+                .disable::<PipelinedRenderingPlugin>(),
         )
         .insert_resource(ScoreBoard::default())
         .add_event::<ScoreEvent>()
-        .add_systems(Startup, (
-            window::setup_camera,
-            paddle::spawn_left_paddle,
-            paddle::spawn_right_paddle,
-            background::spawn_background,
-            ball::spawn_ball,
-            scoreboard::spawn_scoreboard,
-        ))
-        .add_systems(Update, (
-            window::close_on_escape,
-            paddle::move_left_paddle,
-            paddle::move_right_paddle,
-            ball::update_ball,
-            scoreboard::update_scoreboard,
-            scoreboard::update_scoreboard_text,
-            ball::reset_ball_system,
-        ))
+        .add_systems(
+            Startup,
+            (
+                window::setup_camera,
+                paddle::spawn_left_paddle,
+                paddle::spawn_right_paddle,
+                background::spawn_background,
+                ball::spawn_ball,
+                scoreboard::spawn_scoreboard,
+            ),
+        )
+        .add_systems(
+            Update,
+            (
+                window::close_on_escape,
+                paddle::move_left_paddle,
+                paddle::move_right_paddle,
+                ball::update_ball,
+                scoreboard::update_scoreboard,
+                scoreboard::update_scoreboard_text,
+                ball::reset_ball_system,
+            ),
+        )
         .run();
 }
 
